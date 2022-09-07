073c9ab94c1510a91e08a44c926adac0799dd05f
Mira Kim <mira@LAPTOP-QTISC452.localdomain>
2022-09-07 19:42:54 -0400

    hide tailwind configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-        // For the best performance and to avoid false positives,
-        // be as specific as possible with your content configuration.
-    ]
-}
+// module.exports = {
+//     plugins: {
+//         tailwindcss: {},
+//         autoprefixer: {},
+//     },
+//     content: [
+//         './pages/**/*.{js,ts,jsx,tsx}',
+//         './components/**/*.{js,ts,jsx,tsx}',
+//         // For the best performance and to avoid false positives,
+//         // be as specific as possible with your content configuration.
+//     ]
+// }
 
 // To learn more about custom PostCSS configuration, see docs for PostCSS: https://nextjs.org/docs/advanced-features/customizing-postcss-config
 // To get started with Tailwind CSS, see Next.js's Example: https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss
